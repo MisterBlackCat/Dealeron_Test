45fc25040a725c19767211aad43c022b3b583d82
Black <jblack@powertrain.com>
2019-04-10 13:54:51 -0400

    Update ImportedItem.cs

--- DealeronTest/Models/ImportedItem.cs
+++ DealeronTest/Models/ImportedItem.cs
@@ -33,7 +33,7 @@ namespace DealeronTest.Models
             }
             else
             {
-                return itemPrice;
+                return itemPrice + (itemPrice * .05f);
             }
         }
 
